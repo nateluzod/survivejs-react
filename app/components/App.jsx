a6d957cea5ee75639b4bfa742d4eb06b9b86455a
Nate Luzod <nate.luzod@mac.com>
2017-09-02 22:41:11 -1000

    Attach/detach notes to/from correct lane

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,49 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 import uuid from 'uuid';
-import Notes from './Notes';
 import connect from '../libs/connect';
-import NoteActions from '../actions/NoteActions';
+import Lanes from './Lanes';
+import LaneActions from '../actions/LaneActions';
 
-class App extends React.Component {
-  render () {
-    const {notes} = this.props;
-    return (
-      <div className="notes-container">
-        <h1>To-Dos in React</h1>
-        <p>Click a task to edit, press enter to save.</p>
-        <Notes
-          notes={notes}
-          onNoteClick={this.activateNoteEdit}
-          onEdit={this.editNote}
-          onDelete={this.deleteNote} />
-        {this.props.test}
-        <button className="add-note" onClick={this.addNote}>Add Task</button>      
-      </div>    
-    );
-  }
-  
-  addNote = () => {
-    this.props.NoteActions.create({
+const App = ({LaneActions, lanes}) => {
+  const addLane = () => {
+    LaneActions.create({
       id: uuid.v4(),
-      task: 'New task'
+      name: 'New lane'
     });
-  }  
-  
-  deleteNote = (id, e) => {
-    e.stopPropagation();
-    this.props.NoteActions.delete(id);
-  }
+  };
 
-  activateNoteEdit = (id) => {
-    this.props.NoteActions.update({id, editing: true});
-  }
+  return (
+    <div>
+      <button className="add-lane" onClick={addLane}>+</button>
+      <Lanes lanes={lanes} />
+    </div>
+  );
+};
 
-  editNote = (id, task) => {
-    this.props.NoteActions.update({id, task, editing: false});
-  }
-}
-
-export default connect(
-  ({notes}) => ({notes}), 
-  {NoteActions}
-)(App)
+export default connect(({lanes}) => ({
+  lanes
+}), {
+  LaneActions
+})(App)
